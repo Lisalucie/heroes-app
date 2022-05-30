43f07492a0b0bf02df2fd06c969602b6c0419b3b
Audren MARCOS <audren.paillard55@orange.fr>
2022-05-30 16:27:01 +0200

    add the 2 tables : guils_choices & user_quests and foreigns keys

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -1,3 +1,4 @@
 class Quest < ApplicationRecord
   belongs_to :guild
+  has_many :user_quests
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_30_140429) do
+ActiveRecord::Schema.define(version: 2022_05_30_142409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "guild_choices", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "guild_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["guild_id"], name: "index_guild_choices_on_guild_id"
+    t.index ["user_id"], name: "index_guild_choices_on_user_id"
+  end
 
   create_table "guilds", force: :cascade do |t|
     t.string "name"
@@ -34,6 +43,17 @@ ActiveRecord::Schema.define(version: 2022_05_30_140429) do
     t.index ["guild_id"], name: "index_quests_on_guild_id"
   end
 
+  create_table "user_quests", force: :cascade do |t|
+    t.string "status"
+    t.integer "user_occurences"
+    t.bigint "quest_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["quest_id"], name: "index_user_quests_on_quest_id"
+    t.index ["user_id"], name: "index_user_quests_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -50,5 +70,9 @@ ActiveRecord::Schema.define(version: 2022_05_30_140429) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "guild_choices", "guilds"
+  add_foreign_key "guild_choices", "users"
   add_foreign_key "quests", "guilds"
+  add_foreign_key "user_quests", "quests"
+  add_foreign_key "user_quests", "users"
 end
