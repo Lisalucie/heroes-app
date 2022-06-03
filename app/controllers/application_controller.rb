b7b2645d7e7147ae88c63f06136917c03d6f7beb
lisa loreau <lisalor44@gmail.com>
2022-06-03 16:30:24 +0200

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  protect_from_forgery with: :null_session
+  skip_before_action :verify_authenticity_token
 
 
 
