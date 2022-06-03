7a15a06bb9576f85624e3d78f73cd3583d57a925
lisa loreau <lisalor44@gmail.com>
2022-06-03 16:25:32 +0200

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+  protect_from_forgery with: :null_session
+
 
 
   def configure_permitted_parameters
