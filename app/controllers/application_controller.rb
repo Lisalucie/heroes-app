1e4ee28650bf8150547257b5befd98f832d863aa
lisa loreau <lisalor44@gmail.com>
2022-06-06 12:14:26 +0200

    remove navbar without guild choice, form background

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   skip_before_action :verify_authenticity_token
 
-
-
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
@@ -12,4 +10,6 @@ class ApplicationController < ActionController::Base
     # For additional in app/views/devise/registrations/edit.html.erb
     devise_parameter_sanitizer.permit(:account_update, keys: [:nickname])
   end
+
+
 end
