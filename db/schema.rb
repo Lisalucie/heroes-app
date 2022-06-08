ad5ae072fa916c932e06a6ee11a68f99172dadd5
Camille Cantin <camille.cantin02@gmail.com>
2022-06-08 15:01:54 +0200

    add comment to user quests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_04_151225) do
+ActiveRecord::Schema.define(version: 2022_06_08_125906) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,7 @@ ActiveRecord::Schema.define(version: 2022_06_04_151225) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "comment"
     t.index ["quest_id"], name: "index_user_quests_on_quest_id"
     t.index ["user_id"], name: "index_user_quests_on_user_id"
   end
